e5522e24a366ad09042ba81c63617961290f47a3
Ilan Kleiman <ilankleiman@gmail.com>
2017-06-24 16:38:06 -0400

    Create RapidVideo.js
    Removes annoying popup ad when clicking on video to play it

---
+++ RapidVideo.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         RapidVideo No Popup
+// @namespace    http://github.com/shortland
+// @version      0.1
+// @description  Remove popup ads from RapidVideo.com (OnePiece player at least)
+// @author       Ilan Kleiman
+// @match        https://www.rapidvideo.com/e/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+/*
+    Autoplay for RapidVideo.com host
+        &
+    Removes popup ads from RapidVideo.com
+*/
+    var markup = document.documentElement.innerHTML;
+    var uniqueFalsePlay = markup.replace(/(var playerInstance \= jwplayer\(\"home\_video\"\)\.setup\(\{\"autostart\"\:) false,/, "$1true,");
+    var uniqueFalsePlay = markup.replace(/<!-- PopAds\.net Popunder Code for www\.rapidvideo\.com [.|\s|\S]+<\/script>/m, "");
+    document.write(uniqueFalsePlay); // There's definitely a better way :(
+})();
